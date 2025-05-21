1912c5bcecf437b6b64402c779e9080ad9a7ffd4
Alex test <alexanderkorshak4@gmail.com>
2025-05-21 17:32:07 +0300

    Реализовано первое задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,49 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
   let arr = [];
+  const discriminant = b ** 2 - 4 * a * c;
+  
+  if (discriminant < 0) {
+    return arr;
+  } else if (discriminant === 0) {
+    const root = -b / (2 * a);
+    arr.push(root);
+  } else {
+    const sqrtDiscriminant = Math.sqrt(discriminant);
+    const root1 = (-b + sqrtDiscriminant) / (2 * a);
+    const root2 = (-b - sqrtDiscriminant) / (2 * a);
+    arr.push(root1, root2);
+  }
   
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   
+  if (
+    typeof percent !== "number" ||
+    typeof contribution !== "number" ||
+    typeof amount !== "number" ||
+    typeof countMonths !== "number"
+  ) {
+    return false; 
+  }
+
+  
+  const monthlyPercent = percent / 100 / 12;
+
+  
+  const loanBody = amount - contribution;
+
+  
+  const monthlyPayment =
+    loanBody *
+    (monthlyPercent +
+      monthlyPercent / ((1 + monthlyPercent) ** countMonths - 1));
+
+  
+  const totalAmount = parseFloat((monthlyPayment * countMonths).toFixed(2));
+
+  return totalAmount;
 }
