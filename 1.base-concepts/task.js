48319f3af7681eefca9639b485c0825951700671
Alex test <alexanderkorshak4@gmail.com>
2025-05-21 18:32:01 +0300

    Реализовано второе задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,49 +1,21 @@
-"use strict";
-
-function solveEquation(a, b, c) {
-  let arr = [];
-  const discriminant = b ** 2 - 4 * a * c;
-  
-  if (discriminant < 0) {
-    return arr;
-  } else if (discriminant === 0) {
-    const root = -b / (2 * a);
-    arr.push(root);
-  } else {
-    const sqrtDiscriminant = Math.sqrt(discriminant);
-    const root1 = (-b + sqrtDiscriminant) / (2 * a);
-    const root2 = (-b - sqrtDiscriminant) / (2 * a);
-    arr.push(root1, root2);
-  }
-  
-  return arr;
-}
-
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   
-  if (
-    typeof percent !== "number" ||
-    typeof contribution !== "number" ||
-    typeof amount !== "number" ||
-    typeof countMonths !== "number"
-  ) {
-    return false; 
-  }
-
-  
   const monthlyPercent = percent / 100 / 12;
-
   
+
   const loanBody = amount - contribution;
-
   
-  const monthlyPayment =
-    loanBody *
-    (monthlyPercent +
-      monthlyPercent / ((1 + monthlyPercent) ** countMonths - 1));
-
   
-  const totalAmount = parseFloat((monthlyPayment * countMonths).toFixed(2));
-
-  return totalAmount;
+  if (loanBody <= 0) {
+    return 0;
+  }
+  
+  const monthlyPayment = loanBody * 
+    (monthlyPercent + monthlyPercent / (Math.pow(1 + monthlyPercent, countMonths) - 1));
+  
+  
+  const totalAmount = monthlyPayment * countMonths + contribution;
+  
+  
+  return parseFloat(totalAmount.toFixed(2));
 }
