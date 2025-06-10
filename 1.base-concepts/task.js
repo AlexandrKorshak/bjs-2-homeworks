0de22db65afaa3f2893b311af1c77cc92dca430f
Alex test <alexanderkorshak4@gmail.com>
2025-06-10 23:24:44 +0300

    Второе сделанное заново задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,17 +1,23 @@
-"use strict";
+function calculateTotalMortgage(percent, contribution, amount, countMonths) {
 
-function solveEquation(a, b, c) {
-	const discriminant = b ** 2 - 4 * a * c;
+	const monthlyPercent = percent / 100 / 12;
 
-	if (discriminant < 0) {
-		return [];
-	} else if (discriminant === 0) {
-		const root = -b / (2 * a);
-		return [root];
-	} else {
 
-		const root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
-		const root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
-		return [root1, root2];
+	const loanBody = amount - contribution;
+
+
+	if (loanBody <= 0) {
+		return 0;
 	}
+
+
+	const monthlyPayment = loanBody * (monthlyPercent + (monthlyPercent / (Math.pow(1 + monthlyPercent, countMonths) - 1)));
+
+
+	const totalAmount = monthlyPayment * countMonths;
+
+
+	const roundedTotal = parseFloat(totalAmount.toFixed(2));
+
+	return roundedTotal;
 }
