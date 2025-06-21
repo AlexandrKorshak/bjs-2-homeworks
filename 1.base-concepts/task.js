96f8fe6da05dbcb6a5716fe2e96c635c99066d64
Alex test <alexanderkorshak4@gmail.com>
2025-06-22 00:10:58 +0300

    Задание 1-ое

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,3 +1,21 @@
+"use strict";
+
+function solveEquation(a, b, c) {
+	const discriminant = b ** 2 - 4 * a * c;
+
+	if (discriminant < 0) {
+		return [];
+	} else if (discriminant === 0) {
+		const root = -b / (2 * a);
+		return [root];
+	} else {
+
+		const root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+		const root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+		return [root1, root2];
+	}
+}
+
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
 
 	const monthlyPercent = percent / 100 / 12;
